a42738698b838bcfd0b3cd50ad23d6cdf1ed07be
Daniel <daniloviciv@hotmail.com>
2019-05-15 21:14:28 +0200

    initial commit with user connections and disconnections events

---
+++ server/sockets/socket.js
@@ -0,0 +1,39 @@
+const { io } = require('../server');
+
+const { Usuarios } = require('../classes/usuarios');
+
+const usuarios = new Usuarios();
+
+
+io.on('connection', (client) => {
+
+    console.log('Usuario conectado');
+
+    client.on('entrarChat', (data, callback) => {
+
+        if (!data.nombre) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre es necesario'
+            })
+        }
+
+        let personas = usuarios.agregarPersona(client.id, data.nombre);
+
+        client.broadcast.emit('listaPersona', usuarios.getPersonas());
+
+        callback(personas);
+
+    })
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.emit('crearMensaje', { usuario: 'Administrador', mensaje: `${personaBorrada.nombre} abandonó el chat` });
+
+        client.broadcast.emit('listaPersona', usuarios.getPersonas());
+
+    });
+
+});
